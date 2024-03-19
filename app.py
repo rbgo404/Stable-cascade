9edc92482358b549b0fce362108cfa2c8cb042d0
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-20 03:18:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class InferlessPythonModel:
           num_inference_steps=10
       ).images[0]
       buff = BytesIO()
-      image.save(buff, format="JPEG")
+      decoder_output.save(buff, format="JPEG")
       img_str = base64.b64encode(buff.getvalue()).decode()
       return { "generated_image_base64" : img_str }
 
